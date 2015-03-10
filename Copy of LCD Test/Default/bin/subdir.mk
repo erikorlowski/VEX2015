96e1ba8da4c15985aa7aa38a082b6d3e2b4b24fc
erikorlowski <erik.orlowski@gmail.com>
2015-03-10 15:28:27 -0500

    LCD Code and Gyro
    Simple LCD project is stable and tested, currently the LCD code has no
    way of communicating with the rest of the program but this should not be
    difficult to implement, gyro code reads a reasonable value but it has
    not been tested for exactness

---
+++ Copy of LCD Test/Default/bin/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../bin/Drive.o \
+../bin/DriveForTime.o \
+../bin/DriveTestMotor.o \
+../bin/OI.o \
+../bin/PantherMotor.o \
+../bin/TestMotor.o \
+../bin/Utilities.o \
+../bin/auto.o \
+../bin/init.o \
+../bin/opcontrol.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
---
+++ Copy of LCD Test/include/Properties.h
@@ -0,0 +1,19 @@
+/*
+ * Properties.h
+ *
+ *  Created on: Feb 25, 2015
+ *      Author: Erik
+ */
+
+#ifndef PROPERTIES_H_
+#define PROPERTIES_H_
+
+#define BLUE 1
+#define RED 0
+extern int alliance;
+
+#define DO_NOTHING 0
+#define DRIVE_FORWARD_2000 1
+#define MODE DRIVE_FORWARD_2000
+
+#endif /* PROPERTIES_H_ */
